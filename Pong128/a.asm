afa722ff0ddf0e217416bda6a4524e985ce701e4
EWARS2 <jefmacho99@gmail.com>
2025-07-27 11:28:48 -0500

    Autocommit: feat: Position code

--- Pong128/a.asm
+++ Pong128/a.asm
@@ -57,8 +57,9 @@ Clear 	dex
 	
 	
 	
-; Might want to Innit Y here
+; Y still hasn't been init yet
 ; This is also where the copy2RAM code is gonna go
+; when the core game is finished
 
 
 ; This is where the actual code will be run.
@@ -113,6 +114,57 @@ Overscan	sta WSYNC
 ; After this, X=0
 			jmp StartOfFrame
 			
+
+
+; Positions an object horizontally
+; Inputs: A = Desired position.
+; X = Desired object to be positioned (0-5).
+; scanlines: If control comes on or before cycle 73 then 1 scanline is consumed.
+; If control comes after cycle 73 then 2 scanlines are consumed.
+; Outputs: X = unchanged
+; A = Fine Adjustment value.
+; Y = the "remainder" of the division by 15 minus an additional 15.
+; control is returned on cycle 6 of the next scanline.
+PosObject   SUBROUTINE
+            sta WSYNC                ; 00     Sync to start of scanline.
+            sec                      ; 02     Set the carry flag so no borrow will be
+                                     ;        applied during the division.
+.divideby15 sbc #15                  ; 04     Waste the necessary amount of time
+                                     ;        dividing X-pos by 15!
+            bcs .divideby15          ; 06/07  11/16/21/26/31/36/41/46/51/56/61/66
+            tay
+            lda fineAdjustTable,y    ; 13 -> Consume 5 cycles by guaranteeing we
+                                     ;       cross a page boundary
+            sta HMP0,x
+            sta RESP0,x              ; 21/ 26/31/36/41/46/51/56/61/66/71
+                                     ; Set the rough position.
+            rts
+;-----------------------------
+; This table converts the "remainder" of the division by 15 (-1 to -15) to the correct
+; fine adjustment value. This table is on a page boundary to guarantee the processor
+; will cross a page boundary and waste a cycle in order to be at the precise position
+; for a RESP0,x write
+            ORG $FF00
+fineAdjustBegin
+            DC.B %01110000; Left 7 
+            DC.B %01100000; Left 6
+            DC.B %01010000; Left 5
+            DC.B %01000000; Left 4
+            DC.B %00110000; Left 3
+            DC.B %00100000; Left 2
+            DC.B %00010000; Left 1
+            DC.B %00000000; No movement.
+            DC.B %11110000; Right 1
+            DC.B %11100000; Right 2
+            DC.B %11010000; Right 3
+            DC.B %11000000; Right 4
+            DC.B %10110000; Right 5
+            DC.B %10100000; Right 6
+            DC.B %10010000; Right 7
+fineAdjustTable EQU fineAdjustBegin - %11110001; NOTE: %11110001 = -15
+
+
+
 
 	org $FFFA
 InterruptVectors
